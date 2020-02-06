b1012498f4cbec2c2033e20f5de90b17d693f8fb
salkam3156 <salkam3156@hotmail.com>
2020-02-06 22:21:44 +0100

    .

--- src/controllers/controller_base.rs
+++ src/controllers/controller_base.rs
@@ -1,4 +1,12 @@
-trait controller_base {
-    fn new() -> Self;
-    fn authorize() -> bool;
+use log::info;
+
+pub struct controller_base {}
+
+impl controller_base {
+    fn new() -> Self {
+        controller_base {}
+    }
+    fn authorize() -> bool {
+        true
+    }
 }
