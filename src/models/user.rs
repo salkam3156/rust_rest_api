130b717f9348a4589109be28332f318b670b553b
salkam3156 <salkam3156@hotmail.com>
2020-02-06 22:22:54 +0100

    user model seria/deserialization , added test

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,7 +1,32 @@
-#[derive(PartialEq, PartialOrd)]
-pub struct user {
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Debug)]
+pub struct User {
     name: String,
     id: u32,
 }
 
-trait serializable {}
+#[cfg(test)]
+mod serialization_tests {
+    use super::*;
+    #[test]
+    fn should_serialize_deserialize_object() {
+        let test_model = User {
+            name: String::from("Test Model"),
+            id: 32,
+        };
+
+        let serialized_model = serde_json::to_string(&test_model).unwrap();
+        let deserialized_model: User = serde_json::from_str(&serialized_model).unwrap();
+
+        assert_ne!(
+            test_model,
+            User {
+                name: String::from("Not Test Model"),
+                id: 32
+            }
+        );
+
+        assert_eq!(test_model, deserialized_model);
+    }
+}
