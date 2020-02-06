73ef686152650d8533e6810962cf54a1d5f53f05
salkam3156 <salkam3156@hotmail.com>
2020-02-06 21:19:31 +0100

    organizing modules

---
+++ src/controllers/test.rs
@@ -0,0 +1,15 @@
+pub use rocket::http::RawStr;
+use rocket::*;
+
+#[get("/")]
+pub fn index() -> &'static str {
+    "Test"
+}
+#[get("/<param>")]
+pub fn root_route(param: &RawStr) -> String {
+    return format!("{}", param);
+}
+#[get("/<param_nested>")]
+pub fn nest_route(param_nested: &RawStr) -> String {
+    return format!("{}", param_nested.as_str());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
-
 mod controllers;
-use controllers::main_controller;
+use controllers::*;
+use rocket::*;
 
 fn main() {
     rocket::ignite()
-        .mount(
-            "/",
-            routes![main_controller::index, main_controller::root_route],
-        )
-        .mount("/nest/", routes![main_controller::nest_route])
+        .mount("/", routes![test::index, test::root_route])
+        .mount("/nest/", routes![test::nest_route])
         .launch();
 }
