f2a3cba297955b7c340d849f8bfd79a0ea75bcd7
salkam3156 <salkam3156@hotmail.com>
2020-02-07 19:58:53 +0100

    launching and reporting json-server stdout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+use std::process::Command;
+use std::str;
 
 mod controllers;
 use controllers::test_controller;
@@ -9,6 +11,15 @@ use models::*;
 use rocket::*;
 
 fn main() {
+    //TODO: make a configuration model, quit the server after shutting the API down
+    //TODO: log to file
+    let command_string = "json-server --port 8321 --watch db.json";
+    let json_server_launch = Command::new("cmd")
+        .args(&["/C", &command_string])
+        .output()
+        .expect("launching json-server failed");
+    println!("{:#?}", str::from_utf8(&json_server_launch.stdout));
+
     rocket::ignite()
         .mount(
             "/",
