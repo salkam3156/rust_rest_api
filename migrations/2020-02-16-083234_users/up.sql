fc7bb4776948358a2ab2db2f721829c78fce001f
salkam3156 <salkam3156@hotmail.com>
2020-02-16 10:01:43 +0100

    added and ran migration

---
+++ migrations/2020-02-16-083234_users/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+create table users(
+    id int not null,
+    userName text not null,
+    primary key (id)
+);
---
+++ src/schema.rs
@@ -0,0 +1,6 @@
+table! {
+    users (id) {
+        id -> Integer,
+        userName -> Text,
+    }
+}
